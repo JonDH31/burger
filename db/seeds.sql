4f3ccd66e132fa19b00c46c428606e2b442d7be9
Jon Stampley <jonstampley@gmail.com>
2018-08-14 18:23:49 -0500

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Juicy Lucy');
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon');
+INSERT INTO burgers (burger_name) VALUES ('In And Out');
+INSERT INTO burgers (burger_name) VALUES ('Patty Melt');
+INSERT INTO burgers (burger_name) VALUES ('Prime');
